ced046603b25be3c0aeff55a46e8f92e066f394a
Blake W <bkw972@gmail.com>
2023-11-13 14:26:02 -0600

    Add: gen first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponent } from './first/first.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [CommonModule, RouterOutlet, FirstComponent],
 })
 export class AppComponent {
   title = 'angular-course';
