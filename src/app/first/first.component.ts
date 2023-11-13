95898f77b171e82731f6809609afa532e632d305
Blake W <bkw972@gmail.com>
2023-11-13 16:56:05 -0600

    Add: two way binding example

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-first',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './first.component.html',
-  styleUrl: './first.component.scss'
+  styleUrl: './first.component.scss',
 })
 export class FirstComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {}
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
